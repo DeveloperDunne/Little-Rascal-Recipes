1aceb00bb030fc379a1b99191f92cd6905d886b1
carl.dunne <carl.m.dunne@hotmail.com>
2024-04-30 14:16:48 +0100

    Created env.py file and added database

--- little_rascal_recipes/settings.py
+++ little_rascal_recipes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5pv)y*6g1fx@!jvr5qa$wpu--znc^!ah*76hc=8z3@w*x+tnf5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  '.herokuapp.com',]
