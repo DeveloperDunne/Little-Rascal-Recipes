7fb9e6a019414536471160bac85c3f1a7447bc36
carl.dunne <carl.m.dunne@hotmail.com>
2024-05-05 14:53:23 +0100

    Confirmation of contact request set up

--- about/views.py
+++ about/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import ContactUsForm
 # Create your views here.
 
 def about_me(request):
+
+    if request.method == "POST":
+        contact_us_form = ContactUsForm(data=request.POST)
+        if contact_us_form.is_valid():
+            contact_us_form.save()
+            messages.add_message(request, messages.SUCCESS, "Thanks for your message! I will try to read and respond within 5 working days.")
     """
     Renders the About page
     """
