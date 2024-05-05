39c235a6988f6ae3598a05f94697723348d558d9
carl.dunne <carl.m.dunne@hotmail.com>
2024-05-05 14:42:16 +0100

    Craeted Contact form

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import About
+from .forms import ContactUsForm
 # Create your views here.
 
 def about_me(request):
@@ -7,9 +8,11 @@ def about_me(request):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    contact_us_form = ContactUsForm
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+         "contact_us_form": contact_us_form},
     )
