228df95d370d02ac437c4813e53a388f30cb89de
carl.dunne <carl.m.dunne@hotmail.com>
2024-05-08 10:50:50 +0100

    Amended layout to share.html page

--- share/urls.py
+++ share/urls.py
@@ -1,6 +1,6 @@
 from .import views
 from django.urls import path
 urlpatterns = [
-    path('', views.Share,
+    path('', views.share_a_book,
          name='share'),
 ]
--- share/views.py
+++ share/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .models import Share
+from .forms import ShareABookForm
 # Create your views here.
+
+def share_a_book(request):
+
+    if request.method == "POST":
+        share_a_book_form = ShareABookForm(data=request.POST)
+        if share_a_book_form.is_valid():
+            share_a_book_form.save()
+            messages.add_message(request, messages.SUCCESS, "Thanks for sharing!!")
+    """
+    Renders the Contact page
+    """
+    share_a_book_form = ShareABookForm
+
+    return render(
+        request,
+        "share/share.html",
+        {"share_a_book_form": share_a_book_form,}
+    )
