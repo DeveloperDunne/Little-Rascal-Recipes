007ff3fcad4b6505f36a4b14a0d887874247441b
carl.dunne <carl.m.dunne@hotmail.com>
2024-05-08 16:55:14 +0100

    Updated styles.css

--- staticfiles/js/share.js
+++ staticfiles/js/share.js
@@ -1,6 +1,6 @@
 const editButtons = document.getElementsByClassName("btn-edit");
-const commentText = document.getElementById("id_body");
-const commentForm = document.getElementById("commentForm");
+const shareText = document.getElementById("id_body");
+const shareForm = document.getElementById("shareForm");
 const submitButton = document.getElementById("submitButton");
 
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
@@ -9,7 +9,7 @@ const deleteConfirm = document.getElementById("deleteConfirm");
 
 /*
  * Initializes edit functionality for the provided edit buttons.
- * 
+ *
  * For each button in the `editButtons` collection:
  * - Retrieves the associated comment's ID upon click.
  * - Fetches the content of the corresponding comment.
@@ -19,31 +19,29 @@ const deleteConfirm = document.getElementById("deleteConfirm");
  */
 
 for (let button of editButtons) {
-    button.addEventListener("click", (e) => {
-      let commentId = e.target.getAttribute("data-comment_id");
-      let commentContent = document.getElementById(
-        `comment${commentId}`
-      ).innerText;
-      commentText.value = commentContent;
-      submitButton.innerText = "Update";
-      commentForm.setAttribute("action", `edit_comment/${commentId}`);
-    });
+  button.addEventListener("click", (e) => {
+    let shareId = e.target.getAttribute("data-share_id");
+    let shareContent = document.getElementById(`share${shareId}`).innerText;
+    shareText.value = shareContent;
+    submitButton.innerText = "Update";
+    shareForm.setAttribute("action", `edit_share/${shareId}`);
+  });
 }
 
 /*
  * Initializes deletion functionality for the provided delete buttons.
- * 
+ *
  * For each button in the `deleteButtons` collection:
  * - Retrieves the associated comment's ID upon click.
- * - Updates the `deleteConfirm` link's href to point to the 
+ * - Updates the `deleteConfirm` link's href to point to the
  * deletion endpoint for the specific comment.
- * - Displays a confirmation modal (`deleteModal`) to prompt 
+ * - Displays a confirmation modal (`deleteModal`) to prompt
  * the user for confirmation before deletion.
  */
 for (let button of deleteButtons) {
-    button.addEventListener("click", (e) => {
-      let commentId = e.target.getAttribute("data-comment_id");
-      deleteConfirm.href = `delete_comment/${commentId}`;
-      deleteModal.show();
-    });
+  button.addEventListener("click", (e) => {
+    let shareId = e.target.getAttribute("data-share_id");
+    deleteConfirm.href = `delete_share/${shareId}`;
+    deleteModal.show();
+  });
 }
